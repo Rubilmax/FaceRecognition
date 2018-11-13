71b1e7e9bd22e7ef036bd0b20071ac3e7e7f9c43
Romain Milon <romain.milon@student-cs.fr>
2018-11-13 16:21:22 +0100

    Implemented Canny Edge Detection using OpenCV Added docstrings

--- app/utils_cv.py
+++ app/utils_cv.py
@@ -2,7 +2,13 @@ import numpy as np
 import cv2
 
 def display(file):
+    """Prints image and wait for the user to press any key"""
     img = cv2.imread(file,cv2.IMREAD_UNCHANGED)
     cv2.imshow('Image',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def blur(file):
+    """Returns the grayscale edges of the image detected with Canny Edge Detection"""
+    img = cv2.imread(file,cv2.IMREAD_GRAYSCALE)
+    return cv2.Canny(img,100,200)
