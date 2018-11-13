3beb68b9313857fcb0c0314d0ff26ef06cfd4544
Romain Milon <romain.milon@student-cs.fr>
2018-11-13 16:05:05 +0100

    Rearranged files

---
+++ app/camera.py
@@ -0,0 +1,11 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview(alpha=200)
+sleep(10)
+camera.stop_preview()
+
+#camera.rotation
+#camera.capture('image.jpg')
