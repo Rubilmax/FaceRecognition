cb7328b7e6690ab556d233b3fb6fd54262bd5ec5
Romain Milon <romain.milon@student-cs.fr>
2018-11-14 10:59:02 +0100

    Implemented TensorFlow detector using OpenCV

--- app/detectors/ts_detector.py
+++ app/detectors/ts_detector.py
@@ -3,66 +3,69 @@ import cv2
 import time
 import sys
 
+box_color = (0, 255, 0)
+frame_process_size = (300,300)
+conf_threshold = .7
 
-def detect(net, frame):
-    frameOpencvDnn = frame.copy()
-    frameHeight = frameOpencvDnn.shape[0]
-    frameWidth = frameOpencvDnn.shape[1]
-    blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], False, False)
+modelFile = "opencv_face_detector_uint8.pb"
+configFile = "opencv_face_detector.pbtxt"
+net = cv2.dnn.readNetFromTensorflow(modelFile, configFile)
+
+def process(frame):
+    """Returns the boxes list of detected faces in the @frame"""
+    global net
+    out_frame = frame.copy()
+    height = out_frame.shape[0]
+    width = out_frame.shape[1]
+    #We shrink the image down to size @frame_process_size
+    blob = cv2.dnn.blobFromImage(out_frame, 1.0, frame_process_size, [104, 117, 123], False, False)
 
     net.setInput(blob)
     detections = net.forward()
-    bboxes = []
+    boxes = []
     for i in range(detections.shape[2]):
         confidence = detections[0, 0, i, 2]
         if confidence > conf_threshold:
-            x1 = int(detections[0, 0, i, 3] * frameWidth)
-            y1 = int(detections[0, 0, i, 4] * frameHeight)
-            x2 = int(detections[0, 0, i, 5] * frameWidth)
-            y2 = int(detections[0, 0, i, 6] * frameHeight)
-            bboxes.append([x1, y1, x2, y2])
-            cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight / 150)), 8)
-    return frameOpencvDnn, bboxes
+            x1 = int(detections[0, 0, i, 3] * width)
+            y1 = int(detections[0, 0, i, 4] * height)
+            x2 = int(detections[0, 0, i, 5] * width)
+            y2 = int(detections[0, 0, i, 6] * height)
+            boxes.append([x1, y1, x2, y2])
+            cv2.rectangle(out_frame, (x1, y1), (x2, y2), box_color, int(round(height / 150)), 8)
+    return out_frame, boxes
 
-
-if __name__ == "__main__":
-
-    modelFile = "opencv_face_detector_uint8.pb"
-    configFile = "opencv_face_detector.pbtxt"
-    net = cv2.dnn.readNetFromTensorflow(modelFile, configFile)
-
-    conf_threshold = 0.7
-
+def detect():
+    """Detects faces present in the video source and save video to file"""
     source = 0
+    #By default we use 0 but we never know if there's any camera added to device, use it
     if len(sys.argv) > 1:
         source = sys.argv[1]
 
     cap = cv2.VideoCapture(source)
-    hasFrame, frame = cap.read()
+    has_frame, frame = cap.read()
 
-    vid_writer = cv2.VideoWriter('output-dnn-{}.avi'.format(str(source).split(".")[0]),
-                                 cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 15, (frame.shape[1], frame.shape[0]))
+    vid_writer = cv2.VideoWriter('video-save-{}.avi'.format(str(source).split(".")[0]), cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 15, (frame.shape[1], frame.shape[0]))
 
     frame_count = 0
-    tt_opencvDnn = 0
+    tt = 0
     while (1):
-        hasFrame, frame = cap.read()
-        if not hasFrame:
+        has_frame, frame = cap.read()
+        if not has_frame:
             break
         frame_count += 1
 
         t = time.time()
-        outOpencvDnn, bboxes = detect(net, frame)
-        tt_opencvDnn += time.time() - t
-        fpsOpencvDnn = frame_count / tt_opencvDnn
-        label = "OpenCV DNN ; FPS : {:.2f}".format(fpsOpencvDnn)
-        cv2.putText(outOpencvDnn, label, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.4, (0, 0, 255), 3, cv2.LINE_AA)
+        out_frame, boxes = process(frame)
+        tt += time.time() - t
+        fps = frame_count / tt
+        label = "OpenCV DNN ; FPS : {:.2f}".format(fps)
+        cv2.putText(out_frame, label, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.4, (0, 0, 255), 3, cv2.LINE_AA)
 
-        cv2.imshow("Face Detection Comparison", outOpencvDnn)
+        cv2.imshow("Face Detection Comparison", out_frame)
 
-        vid_writer.write(outOpencvDnn)
+        vid_writer.write(out_frame)
         if frame_count == 1:
-            tt_opencvDnn = 0
+            tt = 0
 
         k = cv2.waitKey(10)
         if k == 27:
