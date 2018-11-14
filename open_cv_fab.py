7816ad97924305f4f6d3b1d087bf3c7cf5ded002
Fabien Niglio <fabien.niglio@student-cs.fr>
2018-11-14 10:53:43 +0100

    Clarification du module open_cv_fab

--- open_cv_fab.py
+++ open_cv_fab.py
@@ -2,12 +2,12 @@ import numpy as np
 import cv2
 
 def load_and_display_image(file_name):
-    image=cv2.imread(file_name)
-    cv2.imshow('image',image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    image=cv2.imread(file_name) #Opens the file
+    cv2.imshow('image',image)   #Displays the file
+    cv2.waitKey(0)              #Waits for the user to press a key
+    cv2.destroyAllWindows()     #Close the other windows
 
-def load_and_display_bw(file_name):
+def load_and_display_bw(file_name): #Does the same but displays the image in black and white
     image=cv2.imread(file_name,0)
     cv2.imshow('image',image)
     cv2.waitKey(0)
